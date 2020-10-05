7533c4110ea31ba2902a777ab8b6c6e9253dbaa7
akhila-77 <akhilajulakanti77@gmail.com>
2020-10-05 10:56:38 +0530

    Updates

---
+++ precache-manifest.2288cb57b00886ec4354c8bf9230dbd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e8e235a97b2f9777e47205ecc837d4",
+    "url": "/reactjsresume/index.html"
+  },
+  {
+    "revision": "258d0af8d4a6eb265e4e",
+    "url": "/reactjsresume/static/css/main.3f73169d.chunk.css"
+  },
+  {
+    "revision": "64a85821ddf0323b88ce",
+    "url": "/reactjsresume/static/js/2.756b31e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactjsresume/static/js/2.756b31e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "258d0af8d4a6eb265e4e",
+    "url": "/reactjsresume/static/js/main.5181bd46.chunk.js"
+  },
+  {
+    "revision": "bba692606bfe42524adb",
+    "url": "/reactjsresume/static/js/runtime-main.9d713a6b.js"
+  },
+  {
+    "revision": "55942c0ee78d77f6a78aee6e77ff96a7",
+    "url": "/reactjsresume/static/media/akhila.55942c0e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjsresume/static/media/logo.5d5d9eef.svg"
+  }
+]);
