60e615bb440ad189d1e81e5761ed9e230c19b597
akhila-77 <akhilajulakanti77@gmail.com>
2020-10-05 10:57:17 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
+import Cards from './components/card.js';
+import {BrowserRouter, Route} from 'react-router-dom';
+import resume from './components/Resume'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return(
+      <div className="App">
+      <BrowserRouter>
+      <Route exact path="/" component={Cards}/>
+      <Route exact path="/resume" component={resume}/>
+      </BrowserRouter>
+      </div>
+      );
+  }
 }
 
 export default App;
